d6c53867a33904765002edb6c964239064b30713
anthonysutardja <anthony.sutardja@gmail.com>
2014-04-23 17:51:59 -0700

    finished first jab at models and wrote tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,48 @@
 var assert = require("assert");
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+var models = require("../js/models");
+
+describe('Point', function(){
+  describe('#()', function(){
+    it('Should return new object with point x,y', function(){
+        var p = new models.Point(2, 1);
+        assert.equal(p.x, 2);
+        assert.equal(p.y, 1);
+    });
+  });
+  describe('#toJSON()', function(){
+    it('Should return JSON object with values', function(){
+        var p = new models.Point(2, 1);
+        var o = p.toJSON();
+        assert.equal(o.x, 2);
+        assert.equal(o.y, 1);
+    });
+  });
+});
+
+
+describe('Zone', function(){
+  describe('#()', function(){
+    it('Should return new object with a list of points', function(){
+        var p1 = new models.Point(1, 1);
+        var p2 = new models.Point(1, 2);
+        var p3 = new models.Point(1, 2);
+        var points = [p1, p2, p3];
+        var zone = new models.Zone(points);
+        assert.equal(zone.points.length, 3);
+        assert.equal(zone.points[0], p1);
+        assert.equal(zone.points[1], p2);
+        assert.equal(zone.points[2], p3);
+    });
+  });
+  describe('#toJSON()', function(){
+    it('Should return JSON object with values', function(){
+        var p1 = new models.Point(1, 1);
+        var p2 = new models.Point(1, 2);
+        var p3 = new models.Point(1, 2);
+        var points = [p1, p2, p3];
+        var zone = new models.Zone(points);
+        var o = zone.toJSON();
+        assert.equal(o.points.length, 3);
     });
   });
 });

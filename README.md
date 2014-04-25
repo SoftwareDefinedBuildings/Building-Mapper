674a20dcb36ca7d6d166050fa247d5eaf4b4a44d
anthonysutardja <anthony.sutardja@gmail.com>
2014-04-25 13:54:39 -0700

    added db models

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Check out the website for installation details.
 To run all tests:
 
     $ mocha
+
+## SQL Models
+Make sure you have a folder called tmp in the project directory (ignored by gitignore)
+
+To create the database schema & tables, run the following:
+
+    >>> from app import db
+    >>> db.create_all()
--- app.py
+++ app.py
@@ -1,14 +1,56 @@
+import os
+
 from flask import Flask
 from flask import render_template, url_for, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.jinja_env.globals['static'] = (
     lambda filename: url_for('static', filename=filename))
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.getcwd() + '/tmp/store.db'
+db = SQLAlchemy(app)
+
+
+class Zone(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(80))
+    points = db.Column(db.Text)  # store as a json blob
+    extras = db.Column(db.Text)  # store extra stuff as a blob
+
+    floor_id = db.Column(db.Integer, db.ForeignKey('floor.id'))
+    floor = db.relationship('Floor',
+        backref=db.backref('zones', lazy='dynamic'))
+
+    def __init__(self, label, points, floor, extras=None):
+        self.points = []
+        self.label = label
+        self.floor = floor
+        if extras is None:
+            self.extras = ''
+
+    def __repr__(self):
+        return '<Zone %r>' % self.label
+
+
+class Floor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(50))
+    extras = db.Column(db.Text)  # store extra stuff as a blob
+
+    def __init__(self, label, extras=None):
+        self.label = label
+        if extras is None:
+            self.extras = ''
+
+    def __repr__(self):
+        return '<Floor %r>' % self.label
+
 
 @app.route('/')
 def list_floors():
-    floors = [1, 2, 3, 4]
+    floors = Floor.query.all()
     return render_template('floor_list.html', floors=floors)
 
 
